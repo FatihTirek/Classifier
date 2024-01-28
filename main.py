1e2bbe47eeb40aeb02ca3d5109849712eb79023e
Fatih Tirek <57770079+FatihTirek@users.noreply.github.com>
2024-01-29 00:21:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from os import path, listdir, mkdir, walk
-from shutil import rmtree, move
-from sys import exit
+import os
+import sys
+import shutil
 
 class Classifier:
     def __init__(self, abs_path, dest_path, traverse=False):
@@ -8,24 +8,24 @@ class Classifier:
         self.dest_path = dest_path
         self.traverse = traverse
 
-        if not path.isdir(self.abs_path) or not path.exists(self.abs_path):
+        if not os.path.isdir(self.abs_path) or not os.path.exists(self.abs_path):
             print('Invalid absolute path. Check it again')
-            exit()
-        if not path.isdir(self.dest_path) or not path.exists(self.dest_path):
+            sys.exit()
+        if not os.path.isdir(self.dest_path) or not os.path.exists(self.dest_path):
             print('Invalid destination path. Check it again')
-            exit()
+            sys.exit()
                 
         if self.traverse:
-            for curr_path, _, files in walk(self.abs_path, topdown=True):
+            for curr_path, _, files in os.walk(self.abs_path, topdown=True):
                 self.__run(files, curr_path)
         else:
-            self.__run(listdir(self.abs_path), self.abs_path)
+            self.__run(os.listdir(self.abs_path), self.abs_path)
             
-        rmtree(self.abs_path)
+        shutil.rmtree(self.abs_path)
 
     def __run(self, files, curr_path):
         for file in files:
-            _, ext = path.splitext(file)
+            _, ext = os.path.splitext(file)
             dest_path = None
 
             if ext:
@@ -33,13 +33,13 @@ class Classifier:
             else:
                 dest_path = self.dest_path + "\\Unknown Files"
 
-            if not path.exists(dest_path):
-                mkdir(dest_path)
+            if not os.path.exists(dest_path):
+                os.mkdir(dest_path)
 
-            source = path.join(curr_path, file)
-            destination = path.join(dest_path, file)
+            source = os.path.join(curr_path, file)
+            destination = os.path.join(dest_path, file)
 
-            move(source, destination)
+            shutil.move(source, destination)
 
 abs_path = input('Enter the absolute path of the directory to be scanned: ').strip()
 dest_path = input('Enter the destination path to put classified files: ').strip()
